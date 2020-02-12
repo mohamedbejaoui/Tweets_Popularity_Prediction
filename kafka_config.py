f5244c17fd5994b37cf6c2072d65fca021c6579f
Mohamed Bejaoui <mohamed.bjewii@outlook.fr>
2020-02-12 10:54:46 +0100

    small tweeks

--- kafka_config.py
+++ kafka_config.py
@@ -7,12 +7,14 @@ admin_client = KafkaAdminClient(bootstrap_servers="localhost:9092", client_id='p
 # for now, we only have one broker
 # when possibility to have more brokers is avaialble, change replciation factor to 2
 topics_list = [
-	NewTopic(name='fit_hawkes_params', num_partitions=100, replication_factor=1)
+	NewTopic(name='fit_hawkes_params', num_partitions=100, replication_factor=1),
+	NewTopic(name='rf_train', num_partitions=100, replication_factor=1)
 ]
 
 # if topic doesn't exist create it, else delete it and recreate it 
-try:
-	admin_client.create_topics(new_topics=topics_list)
-except TopicAlreadyExistsError:
-	admin_client.delete_topics([topic.name for topic in topics_list])
-	admin_client.create_topics(new_topics=topics_list)
+for topic in topics_list:
+	try:
+		admin_client.create_topics(new_topics=[topic])
+	except TopicAlreadyExistsError:
+		admin_client.delete_topics([topic.name])
+		admin_client.create_topics(new_topics=[topic])

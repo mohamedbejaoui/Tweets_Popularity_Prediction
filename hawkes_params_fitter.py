c7e66ea3eb1136585dd2142be52054c4cfa1bf92
Mohamed Bejaoui <mohamed.bjewii@outlook.fr>
2020-03-17 17:14:47 +0100

    include hawkes params fitting and alert feature after prediction

--- hawkes_params_fitter.py
+++ hawkes_params_fitter.py
@@ -1,7 +1,8 @@
 import json
+import numpy as np
 
 from kafka import KafkaConsumer, KafkaProducer
-
+from hawkes_point_process.marked_hawkes import create_start_points, fit_parameters, get_total_events
 
 """
 	Kafka consumer that group received tweets into cascades, 
@@ -13,7 +14,9 @@ from kafka import KafkaConsumer, KafkaProducer
 """
 
 
-CASCADE_OBSERVATION_TIME = 300 # 5min
+CASCADE_OBSERVATION_TIME = 600 # 10min
+ALERT_THRESH = 200 # we send an alert if a cascade size estimtation is bigger than this threshold
+SEED = 0
 
 consumer = KafkaConsumer('fit_hawkes_params', 
 						  bootstrap_servers='localhost:9092', 
@@ -32,23 +35,39 @@ producer = KafkaProducer(bootstrap_servers='localhost:9092',
 cascades_info = {} # dict that stores params of each cascades to be fitted
 fitted_cascades = set()
 
+# create starting params for fitting hawkes process. same seed means same starting point for all cascades
+start_params = np.array([np.random.choice(param) for param in create_start_points(seed=None).values()])
+
 for tweet_info in consumer:
-	num_cascade = tweet_info.key.decode('utf-8')
-	cascades_info[num_cascade] = cascades_info.get(num_cascade, []) + [tweet_info.value]
+	# get cascade number from message key
+	num_cascade = tweet_info.key.decode('utf-8').split('-')[0]
+	# add received tweet's info to the current cascade history. First row of a cascade contains (None, None) that must be ignored before fitting the hawkes params
+	cascades_info[num_cascade] = np.concatenate((cascades_info.get(num_cascade, np.array([None, None]).reshape((1, 2))), 
+												 np.array([tweet_info.value['magnitude'], tweet_info.value['time']]).reshape((1, 2))), axis=0)
 
-	if len(cascades_info[num_cascade]) >= 50 and \
-		cascades_info[num_cascade][-1]['time'] >= CASCADE_OBSERVATION_TIME and \
-		num_cascade not in fitted_cascades:
+	# As soon as we a received tweet's timestamp reaches the defined obsevation time, we fit the hawkes params and ignore upcoming tweets of same cascade 
+	if cascades_info[num_cascade][-1, 1] >= CASCADE_OBSERVATION_TIME and num_cascade not in fitted_cascades:
+		# fit hawkes params using current cascade's history
+		history = cascades_info[num_cascade][1:]
+		# if last tweet's timestamp is > observation_time, remove it
+		if history[-1, 1] > CASCADE_OBSERVATION_TIME:
+			history = history[:-1, :]
 
-		# fit hawkes params for this cascade
-		# TODO: implement real optimizer for params fitting. For now it's just a dummy one
-		fitted_hawkes_params = cascades_info[num_cascade][-1]['time'] * cascades_info[num_cascade][-1]['magnitude']
-
+		print(f"Fitting hawkes parameters for {num_cascade}")
+		result_params, _ = fit_parameters(history.astype(float), start_params)
+		# store fitted params in a dict
+		fitted_params = dict(K=result_params[0], beta=result_params[1], c=result_params[2], theta=result_params[3])
+		# add fitted params to set of cascade nums for which hawkes params were already fitted.
 		fitted_cascades.add(num_cascade)
 
-		print({'feature': fitted_hawkes_params, 'ts': cascades_info[num_cascade][-1]['time'], 'cascade_idx': num_cascade})
+		# calculate the estimated final cascade size
+		N_pred = get_total_events(history=history, T=CASCADE_OBSERVATION_TIME, params=fitted_params)['total']
+		# TODO: add RF prediction layer
 
-		# send the fitted params for a topic to train the RF model
-		producer.send(topic='rf_train', 
-					  value={'feature': fitted_hawkes_params, 'ts': cascades_info[num_cascade][-1]['time'], 'cascade_idx': num_cascade})
+		# if the estimated size of the cascade is > 50, send an alert to a topic
+		if N_pred >= 200:
+			print(f"Sending alert for {num_cascade}")
+			producer.send(topic='pred_size_alert',
+						  value={'cascade_idx': num_cascade, 'estimated_size': N_pred})
+
 		producer.flush()
--- tweets_data_reader.py
+++ tweets_data_reader.py
@@ -4,13 +4,11 @@ import json
 
 from kafka import KafkaProducer
 
-train = True # TODO: change into variable that can be modified when calling this python script
 KAFKA_TOPIC = 'fit_hawkes_params'
 
 # read tweets data
 tweets_dir = './data'
 data_df = pd.read_csv(tweets_dir+'/data.csv')
-data_df.index += 1
 index_df = pd.read_csv(tweets_dir+'/index.csv')
 
 tweets_sent_count = {cascade_idx: 0 for cascade_idx in index_df.index}
@@ -21,23 +19,23 @@ producer = KafkaProducer(bootstrap_servers='localhost:9092',
 
 # just for test, remove later
 tweets_counter = 0
-max_tweets = 1000
+max_tweets = 1000000
 
 while tweets_sent_count != {} and tweets_counter < max_tweets:
 	cascade_idx = random.choice(list(tweets_sent_count.keys()))
-	tweet_idx = tweets_sent_count[cascade_idx] + index_df.iloc[cascade_idx]['start_ind']
+	tweet_idx = tweets_sent_count[cascade_idx] + index_df.iloc[cascade_idx]['start_ind'] - 1
 	tweets_sent_count[cascade_idx] += 1
 	tweet_info = data_df.iloc[tweet_idx].to_dict()
-	# if last tweet in chosen cascade is sent, remove cascade from data we sample from and send end cascade info
-	if tweets_sent_count[cascade_idx] == index_df.iloc[cascade_idx]['end_ind'] - index_df.iloc[cascade_idx]['start_ind']:
+	# if last tweet in chosen cascade is sent, remove cascade from data we sample from
+	N_real = index_df.iloc[cascade_idx]['end_ind'] - index_df.iloc[cascade_idx]['start_ind'] + 1
+	if tweets_sent_count[cascade_idx] == N_real:
 		del tweets_sent_count[cascade_idx]
-		if train:
-			tweet_info['isend_cascade'] = True
 
 	# send sampled tweet's info
 	producer.send(topic=KAFKA_TOPIC,
 				  value=tweet_info,
-				  key=f'cascade_n{cascade_idx+1}'.encode('utf-8'))
+				  # TODO: modify key when initial training dataset is constructed
+				  key=f'cascade_n{cascade_idx+1}-{N_real}'.encode('utf-8'))
 
 	tweets_counter += 1
 

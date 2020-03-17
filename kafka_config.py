9d26a61cd4c7db07b4737105a2e5ff3802e93a3a
Mohamed Bejaoui <mohamed.bjewii@outlook.fr>
2020-03-17 17:12:44 +0100

    add creation of new topic alert receiver

--- kafka_config.py
+++ kafka_config.py
@@ -1,5 +1,6 @@
 from kafka.admin import KafkaAdminClient, NewTopic
 from kafka.errors import TopicAlreadyExistsError
+import time
 
 
 admin_client = KafkaAdminClient(bootstrap_servers="localhost:9092", client_id='pfe2019')
@@ -8,13 +9,13 @@ admin_client = KafkaAdminClient(bootstrap_servers="localhost:9092", client_id='p
 # when possibility to have more brokers is avaialble, change replciation factor to 2
 topics_list = [
 	NewTopic(name='fit_hawkes_params', num_partitions=100, replication_factor=1),
-	NewTopic(name='rf_train', num_partitions=100, replication_factor=1)
+	NewTopic(name='rf_train', num_partitions=100, replication_factor=1),
+	NewTopic(name='pred_size_alert', num_partitions=20, replication_factor=1)
 ]
 
-# if topic doesn't exist create it, else delete it and recreate it 
+# if topic doesn't exist create it 
 for topic in topics_list:
 	try:
 		admin_client.create_topics(new_topics=[topic])
 	except TopicAlreadyExistsError:
-		admin_client.delete_topics([topic.name])
-		admin_client.create_topics(new_topics=[topic])
+		pass

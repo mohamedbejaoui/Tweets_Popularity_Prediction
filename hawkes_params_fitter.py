22584dda9d4be7e3e50830064241f2fb9ac335b2
Mohamed Bejaoui <mohamed.bjewii@outlook.fr>
2020-03-22 23:20:46 +0100

    add feature to check whether old received cascades are finished or not

--- hawkes_params_fitter.py
+++ hawkes_params_fitter.py
@@ -1,23 +1,27 @@
 import json
 import numpy as np
 import requests
+import time
+import pickle
+import csv
 
 from kafka import KafkaConsumer, KafkaProducer
 from hawkes_point_process.marked_hawkes import create_start_points, fit_parameters, get_total_events
 
 
 """
-	Kafka consumer that group received tweets into cascades, 
-	fits the hawkes parameters for each received cascades 
-	(if cascade size > 50 and T is reached)
-	and then either send the cascades to a topic for training
-	or predicts the cascades size using the latest trained RF model(inference)
-	and send an alert to a topic if the predicted size of a cascade is > thresh
+    Kafka node that receives tweets from fit_hawkes_params topic.
+    If observation time for a cascade is reached, predict its final size.
+    If it's bigger than a threshold, send and alert.
+    ALso, this node checks whether old received cascades can be considered finished or not.
+    If they are, we fit their hawkes params and save them into the rf training dataset
 """
 
 
 CASCADE_OBSERVATION_TIME = 600 # 10min
 ALERT_THRESH = 200 # we send an alert if a cascade size estimtation is bigger than this threshold
+NUM_CASCADES_TO_CHECK = 10
+CASCADE_FINISH_DURATION = 3600 # if for a certain cascade, we don't receive a tweet after this duration, we consider it's finished (set to 1h)
 
 consumer = KafkaConsumer('fit_hawkes_params', 
 						  bootstrap_servers='localhost:9092', 
@@ -33,13 +37,15 @@ producer = KafkaProducer(bootstrap_servers='localhost:9092',
 						 value_serializer=lambda x: json.dumps(x).encode('utf-8'))
 
 ## FLASK API where Random Forest model is stored
-url = 'http://0.0.0.0:5000/api/'
-headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+URL = 'http://0.0.0.0:5000/api/'
+HEADERS = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
 
 
 
 cascades_info = {} # dict that stores params of each cascades to be fitted
-fitted_cascades = set()
+cascades_last_ts = {} # stores for each cascade last time a tweet is received
+fitted_cascades = set() # stores fitted cascades numbers. Cascade which number is stored in this set won't be trated anymore
+cascades_for_train = [] # every time a tweet is received, we look into the oldest NUM_CASCADES_TO_CHECK cascades. for each, one we check if we can consider a cascade as finished or not
 
 # create starting params for fitting hawkes process. same seed means same starting point for all cascades
 start_params = np.array([np.random.choice(param) for param in create_start_points(seed=None).values()])
@@ -50,6 +56,9 @@ for tweet_info in consumer:
     # add received tweet's info to the current cascade history. First row of a cascade contains (None, None) that must be ignored before fitting the hawkes params
     cascades_info[num_cascade] = np.concatenate((cascades_info.get(num_cascade, np.array([None, None]).reshape((1, 2))),
               np.array([tweet_info.value['magnitude'], tweet_info.value['time']]).reshape((1, 2))), axis=0)
+    cascades_last_ts[num_cascade] = time.time()
+    cascades_for_train.append(num_cascade)
+
 
     # As soon as we a received tweet's timestamp reaches the defined obsevation time, we fit the hawkes params and ignore upcoming tweets of same cascade 
     if cascades_info[num_cascade][-1, 1] >= CASCADE_OBSERVATION_TIME and num_cascade not in fitted_cascades:
@@ -72,13 +81,13 @@ for tweet_info in consumer:
             pass
         else:
             # RF prediction layer
-            RF_params = [[result_params[2], result_params[3], prediction['a1'], prediction['n_star']]]
-            json_data = json.dumps(RF_params)
-            r = requests.post(url, data=json_data, headers=headers)
+            rf_features = json.dumps([[result_params[2], result_params[3], prediction['a1'], prediction['n_star']]])
+            r = requests.post(URL, data=rf_features, headers=HEADERS)
             r_json = r.json()
             
-            # Output : swaling factor w
-            w_pred = float(r_json[1:-1])
+            # Output : scaling factor w
+            w_pred = float(json.loads(r_json[1:-1])[0])
+            # imrpove cascade size estimation using predicted scaling factor
             N_pred = len(history) + w_pred * prediction['a1'] / (1 - prediction['n_star'])
             
             # if the estimated size of the cascade is > 50, send an alert to a topic
@@ -87,4 +96,27 @@ for tweet_info in consumer:
                 producer.send(topic='pred_size_alert',
                               value={'cascade_idx': num_cascade, 'estimated_size': N_pred})
                 producer.flush()
-#
+
+    # check if we can consider some cascades as finished. If yes, we fit the hawkes params and save them in the RF training dataset
+    cascade_idx_to_remove = []
+    for i in range(NUM_CASCADES_TO_CHECK):
+        if time.time() - cascades_last_ts[cascades_for_train[i]] >= CASCADE_FINISH_DURATION:
+            # include only cascades whith more than 50 tweets in the training dataset
+            cascade_len = len(cascades_info[cascades_for_train[i]])
+            if  cascade_len >= 50:
+                # fit hawkes params using current cascade's history
+                history = cascades_info[num_cascade][1:]
+                history = history[history[: 1] <= CASCADE_OBSERVATION_TIME]
+                result_params, _ = fit_parameters(history.astype(float), start_params)
+                fitted_params = dict(K=result_params[0], beta=result_params[1], c=result_params[2], theta=result_params[3])
+                other_prams = get_total_events(history=history, T=CASCADE_OBSERVATION_TIME, params=fitted_params)
+                rf_features = [result_params[2], result_params[3], other_prams['a1'], other_prams['n_star']]
+                w_train = (cascade_len - len(history)) * (1 - n_star) / other_prams['a1']
+                # write cascade info at the end of rf training dataset csv file
+                with open('./data/rf_train/training_data_rf.csv', 'a') as data_rf:
+                    writer = csv.writer(data_rf)
+                    writer.writerow(rf_features + [w_train, cascade_len])
+            cascade_idx_to_remove.append(i)
+    # remove cascades that are saved for training from the list to check from
+    for i in cascade_idx_to_remove:
+        cascades_for_train.pop(i)

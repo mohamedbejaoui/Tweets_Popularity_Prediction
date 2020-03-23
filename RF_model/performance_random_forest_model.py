f5e354e0be35f483a7a8f47fa70241378e1c8f29
Quentin <quentin.naudan@supelec.fr>
2020-03-24 00:51:24 +0100

    perfs RF

--- RF_model/performance_random_forest_model.py
+++ RF_model/performance_random_forest_model.py
@@ -8,17 +8,12 @@ import pickle
 data = pd.read_csv('training_data_RF.csv')
 
 # CLEAN
-data.drop(data[(data.w>5) | (data.w<0.01)].index, inplace=True)
+data.drop(data[(data.w>5) | (data.w<0.01) | (data.n<10)].index, inplace=True)
 
-
-data.drop(data[data.c>500].index, inplace=True)
-
-X, y = data[['c','theta','A1','n_star']], data[['w']]
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
 # Our performance metric is the average Absolute Relative Error (ARE)
-def mean_ARE(y_pred, y_test):
-    y_real = y_test.to_numpy()
+# Entry variables are DataFrames
+def mean_ARE(y_pred, y_real):
     assert len(y_pred)==len(y_real)
     S = 0.
     for i in range(len(y_pred)):
@@ -26,32 +21,36 @@ def mean_ARE(y_pred, y_test):
     return (S/len(y_pred))
 
 
+res = pd.DataFrame(columns = ['perc', 'size', 'mean_are', 'explnd_var','r2','mae','mse'])
 
-m = RandomForestRegressor(n_estimators = 100,
-       criterion="mae", # Mean-Square Error; or "mae" Mean Absolute Error
-       max_depth = None,
-       min_samples_split=2,
-       min_samples_leaf=1,
-       min_weight_fraction_leaf=0.,
-       max_features="auto",
-       max_leaf_nodes = None,
-       min_impurity_decrease=0.,
-       min_impurity_split=None,
-       bootstrap=True,
-       oob_score=False,
-       n_jobs=None,
-       random_state=None,
-       verbose=0)
-m.fit(X_train,y_train)
-#y_pred = m.predict(X_test)
-#print("\n\nTEST SIZE : {data}".format(data = 0.1+size/10))
-#print("\n#### average ARE : {d}".format(d = mean_ARE(y_pred,y_test)))
-
-#print("#### explained_variance_score :", explained_variance_score(y_pred,y_test))
-#print("\n\n#### r2_score :", r2_score(y_pred,y_test))
-#print("\n\n#### mean_absolute_error :", mean_absolute_error(y_pred,y_test))
-#print("\n\n#### mean_squared_error :", mean_squared_error(y_pred,y_test))
-
+for perc in range(2, 11):
+    sub_data = data.sample(frac = perc/10)
+    X, y = sub_data[['c','theta','A1','n_star']], sub_data[['w']]
+    for siz in range(5):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1+siz/10)
+        m = RandomForestRegressor(n_estimators = 200,
+               criterion="mae", # Mean-Square Error; or "mae" Mean Absolute Error
+               max_depth = None)
+        m.fit(X_train, y_train.w.ravel())
+        y_pred = m.predict(X_test)
+        #print("\n#### average ARE : {d}".format(d = mean_ARE(y_pred,y_test)))
+        # MEAN ARE
+        Idx = y_test.index
+        y_pred = pd.DataFrame(y_pred, index=Idx, columns=['w_pred'])
+        data.loc[Idx, 'N_pred'] = round(data.loc[Idx, 'n'] + y_pred.w_pred * data.loc[Idx, 'A1'] / (1-data.loc[Idx, 'n_star'])).astype(int)
+        data.loc[Idx, 'are'] = abs(data.loc[Idx, 'N_real'] - data.loc[Idx, 'N_pred']) / data.loc[Idx, 'N_real']
+        
+        res.loc[str(perc)+"-"+str(siz)] = {'perc':perc, 'size':siz,
+                'mean_are': data.are.mean(),
+                'explnd_var':explained_variance_score(y_pred,y_test),
+                'r2':r2_score(y_pred,y_test),
+                'mae':mean_absolute_error(y_pred,y_test),
+                'mse':mean_squared_error(y_pred,y_test)}
+        #print("\n\n#### explained_variance_score :", explained_variance_score(y_pred,y_test))
+        #print("\n\n#### r2_score :", r2_score(y_pred,y_test))
+        #print("\n\n#### mean_absolute_error :", mean_absolute_error(y_pred,y_test))
+        #print("\n\n#### mean_squared_error :", mean_squared_error(y_pred,y_test))
+print(res)
       
 ## save model using pickle serializer
-pickle.dump(m, open("RF_model.pickle", 'wb'))
+#pickle.dump(m, open("RF_model.pickle", 'wb'))
--- RF_model/training_data_random_forest.py
+++ RF_model/training_data_random_forest.py
@@ -34,8 +34,8 @@ observation_duration = 600 # seconds = 10 mins
 start_params = np.array([np.random.choice(param) for param in create_start_points(0).values()]) #np.array([.8, .6, 100, .8])
 
 ## TRAINING DATA FOR RANDOM FOREST
-RF_train_data = pd.read_csv('training_data_RF.csv', names=['c','theta','A1','n_star','w','N_real','N_predict','Error_predict','start_ind'], header=0)
-#RF_train_data = pd.DataFrame(columns=['c','theta','A1','n_star','w','N_real','N_predict','Error_predict'])
+RF_train_data = pd.read_csv('training_data_RF.csv', names=['c','theta','A1','n_star','w','N_real','N_predict','Error_predict','n','start_ind'], header=0)
+#RF_train_data = pd.DataFrame(columns=['c','theta','A1','n_star','w','N_real','N_predict','n','Error_predict'])
 
 
 ## DATA CLEANING
@@ -69,8 +69,9 @@ for occ in range(occurences):
         	                           N_real = len(cascade),
         	                           N_predict = prediction['total'],
         	                           Error_predict = 100 * abs(len(cascade) - prediction['total']) / len(cascade),
+                                    n = len(history),
                                     start_ind = indexes.iloc[cascade_idx, 0])
         	        RF_train_data.at[index_start+occ*index_length+cascade_idx, :] = RF_features
     RF_train_data.to_csv('training_data_RF.csv', index=False)
-    RF_train_data = pd.read_csv('training_data_RF.csv', names=['c','theta','A1','n_star','w','N_real','N_predict','Error_predict','start_ind'], header=0)
+    RF_train_data = pd.read_csv('training_data_RF.csv', names=['c','theta','A1','n_star','w','N_real','N_predict','Error_predict','n','start_ind'], header=0)
 ##
